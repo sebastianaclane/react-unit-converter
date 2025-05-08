45f7b40f3f699c9e8a6013955ae3c2a30d097d3f
sebastianaclane <sebastianaclane@gmail.com>
2025-05-07 20:58:11 -0700

    Adding Menu component with length, width and temperature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Menu from './Menu';
 
 function App() {
   
   return (
     <>
       <h1>Unit Converter</h1>
+      <Menu />
       {/* <Length /> */}
       {/* <Weight /> */}
       {/* <Temperature /> */}
---
+++ src/Menu.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Menu() { 
+    return (
+        <ul className="menu grid md:grid-cols-3 gap-4">
+            <li><a data-unit-category="length">Length</a></li>
+            <li><a data-unit-category="weight">Weight</a></li>
+            <li><a data-unit-category="temperature">Temperature</a></li>
+        </ul>
+    );
+}
+
+export default Menu;

e3efb93f1fb823b686399cfda7ca16b0141d4a52
sebastianaclane <sebastianaclane@gmail.com>
2025-05-07 23:52:53 -0700

    Build handleMenuClick function to change type of converter state

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 
-function Menu() { 
+function Menu( {changeTypeOfConverter} ) { 
+    const handleMenuClick = event => {
+        // 1. Stop the link from changing page
+        event.preventDefault();
+
+        let previousConverterLink = document.querySelector('.menu .active');
+        let nextConverterLink = event.currentTarget;
+
+        // 2. Change to a different converter if previous converter and next converter aren't the same
+        if (previousConverterLink != nextConverterLink) {
+            // 3. Remove "active" class from previous link
+            previousConverterLink.classList.remove("active");
+            // 4. Add "active" class to new selected link
+            nextConverterLink.classList.add("active");
+            // 5. Change state to a different type of converter
+            changeTypeOfConverter(nextConverterLink.dataset.unitCategory);
+        }
+    }
+
     return (
         <ul className="menu text-center grid md:grid-cols-3 gap-4">
-            <li><a className="active" data-unit-category="length">Length</a></li>
-            <li><a data-unit-category="weight">Weight</a></li>
-            <li><a data-unit-category="temperature">Temperature</a></li>
+            <li><a className="active" data-unit-category="length" onClick={handleMenuClick}>Length</a></li>
+            <li><a data-unit-category="weight" onClick={handleMenuClick}>Weight</a></li>
+            <li><a data-unit-category="temperature" onClick={handleMenuClick}>Temperature</a></li>
         </ul>
     );
 }

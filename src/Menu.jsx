8c2cda7156dadc4e208664bd286758b1bb10ce9c
sebastianaclane <sebastianaclane@gmail.com>
2025-06-20 16:57:17 -0700

    Add animate-pulse tailwind animation to menu links

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -9,8 +9,8 @@ function Menu( {setTypeOfConverter} ) {
         let nextConverterLink = event.currentTarget;
         // Change to a different converter if previous converter and next converter aren't the same
         if (previousConverterLink != nextConverterLink) {
-            previousConverterLink.classList.remove("active");
-            nextConverterLink.classList.add("active");
+            previousConverterLink.classList.remove("active", "animate-pulse");
+            nextConverterLink.classList.add("active", "animate-pulse");
             // Change state to a different type of converter
             setTypeOfConverter(nextConverterLink.dataset.unitCategory);
         }
@@ -18,9 +18,9 @@ function Menu( {setTypeOfConverter} ) {
 
     return (
         <ul className="menu text-center grid md:grid-cols-3 gap-4">
-            <li><a className="active" data-unit-category="length" onClick={handleMenuClick}>Length</a></li>
-            <li><a data-unit-category="weight" onClick={handleMenuClick}>Weight</a></li>
-            <li><a data-unit-category="temperature" onClick={handleMenuClick}>Temperature</a></li>
+            <li><a className="active animate-pulse" data-unit-category="length" onClick={handleMenuClick}><strong>Length</strong></a></li>
+            <li><a data-unit-category="weight" onClick={handleMenuClick}><strong>Weight</strong></a></li>
+            <li><a data-unit-category="temperature" onClick={handleMenuClick}><strong>Temperature</strong></a></li>
         </ul>
     );
 }
